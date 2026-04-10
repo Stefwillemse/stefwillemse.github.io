2f5bd6f4a994c353f8bfd3c6afa0b33ffe6eecbd
Stef Willemse <stefwillemse@live.nl>
2026-04-10 10:10:20 +0200

    Javascript for grabbing of timeline on PC
    - Added timeline horizontal grab behaviour

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.timeline');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+container.addEventListener('mousedown', (e) => {
+  isDown = true;
+  container.classList.add('active');
+  startX = e.pageX - container.offsetLeft;
+  scrollLeft = container.scrollLeft;
+});
+
+container.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+container.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+container.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - container.offsetLeft;
+  const walk = (x - startX) * .6; // scroll speed multiplier
+  container.scrollLeft = scrollLeft - walk;
+});
